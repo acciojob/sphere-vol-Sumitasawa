2965c0f61d2d38f21368fc83c059357226bd249c
Sumitasawa <132041180+Sumitasawa@users.noreply.github.com>
2025-08-11 13:35:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
-  
-} 
+	
+	   if (event) event.preventDefault();
 
+		let radius=parseFloat(document.getElementById('radius').value);
+
+	if(isNaN(radius)||radius<=0){
+		//alert("Please enter a valid positive number for radius.")
+		return false;
+	}
+
+	let volume=(4/3)*Math.PI*Math.pow(radius,3);
+		document.getElementById('volume').value=volume.toFixed(4);
+	return false;
+
+	
+} 
+	
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
